05c20a02849689ae6a73dd44c9dcc0c182302324
Paul Collantes <jeffersoncollantes31@gmail.com>
2020-09-30 14:55:10 -0500

    Login simple

--- fixedbeams-backend/src/app.controller.ts
+++ fixedbeams-backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import {Body, Controller, Get, Post, Res, Session} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,6 +10,40 @@ export class AppController {
     @Res() res
   ): string {
     return res.render("app/app-component");
-  }  
-    
+  }
+  @Get("login")
+  login(
+      @Res() res
+  ){
+    return res.render("login/login");
+  }
+  @Post("login")
+  loginPost(
+      @Body() parametrosCuerpo,
+      @Res() res,
+      @Session() session
+  ){
+    const autenticado = this.autenticarUsuario(parametrosCuerpo);
+    if(autenticado){
+      session.usuario = parametrosCuerpo.usuario;
+      return res.redirect("category/products");
+    } else {
+      return res.render(
+          "login/login",
+          {error: "Usuario o contraseña no coinciden"}
+      );
+    }
+
+  }
+  autenticarUsuario(parametrosCuerpo):boolean{
+    const usuario = parametrosCuerpo.usuario;
+    const password = parametrosCuerpo.password;
+    if(usuario == "Adrian" && password == "1234"){
+      return true;
+    }
+    return false;
+  }
+
+
+
 }
